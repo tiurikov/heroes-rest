c8713755edcda84a33a510af99353dbd3223dacc
Stanislav Tyurikov <stanislav.tyurikov@gmail.com>
2015-10-22 22:24:07 +0300

    add gradle wrapper for portable builds

--- src/main/java/com/heroes/Starter.java
+++ src/main/java/com/heroes/Starter.java
@@ -8,8 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Stanislav Tyurikov
  */
 @SpringBootApplication
-public class Starter {
-    public static void main(String[] args) {
+public class Starter
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(Starter.class, args);
     }
 }
